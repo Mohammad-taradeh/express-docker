a2914b8d02309c6e02f8cb6fc4adab880ef2f7e2
Mohammad-taradeh <98043145+Mohammad-taradeh@users.noreply.github.com>
2023-09-24 20:45:47 +0300

    it may work

--- infrastructure/setup-instance.sh
+++ infrastructure/setup-instance.sh
@@ -28,8 +28,7 @@ sudo usermod --append --groups docker github
 sudo usermod --append --groups docker ubuntu
 sudo chown github:github -R /home/app
 
-github_pubkey='ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC7HQZSqZgYv3ieNiWlxTU4TcQHdeQ0oQDr5CAYsG3s4iLB9TK7o8uaooyMeJoJHSHhwMgGA29rErc/vtx6Md/LRsKd1w+hlqXPqbqHgbcO8PA91mk0GUmJaO/Ht5U0eIq8IBOzysRz4V76HmD9tiKY7HJeSOW4FU80xryC097qLKo/GWU9f/KOYssX5dPv9By/T/hG41lIevdv8BkWlHdSp3Xk8n8OyvbRk2cYOYr11ZyACsm2Ce2q16aCqctXAbNE6rgaTHCAcYzcDHQoSRNwVT2jTux5ADOTUSxC6+GDqF/IWP4AeoQhME+GpMoUNB9tCp9gAqyCMRD6Y4onwUuLco77XEqwiGNj9aINcMj3IL/swJnqliAXarj5UnH67DLyM4TWZOiVOhYdYoBeqK5Kieyhum+F3p0+dqDWm8A3k6O8TZ8tjTnw6vQNqebdNoWcJGu7UlsRrfAz97p6YthyUSw5hJcExEURIUNhb528fFs2LHOGowzDNTW8SGmBTAKnA8vE4SHEAkSzvR2LE1Yj1DGFJfHQUyppG5HV+YYgI/O++T6Ez49cw0e7XhOmkHzNPJOhCh1Hytsm9/hLwAKrcghtqTf7TwKBLjC5s7aYUxEuFaptW71+3opV9PJUcggSHj6DJaHOcd8IWI3trSCkCnLYkFvXV4YMGWTAvKBE5w== 201160@ppu.edu.ps~
-'
+github_pubkey='ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIA+e5aQj+0z70kAN9kuODNxlT7IYuq/kmUCsavI8duc 201160@ppu.edu.ps.com'
 
 sudo -u github sh -c "mkdir -p /home/app/.ssh && echo $github_pubkey > /home/app/.ssh/authorized_keys"
 
